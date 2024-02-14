3dfcf5b053e9d47081681226d7e882ea6ece4d0e
Ariel Leonardo Luna <ariel.leonardo.luna@gmail.com>
2024-02-14 20:27:49 -0300

    MAIN

---
+++ app.js
@@ -0,0 +1,51 @@
+//obtener express
+const express = require('express');
+
+//Obtener el ProductManager
+const ProductManager = require('./ProductManager');
+
+//App y PORT
+const app = express();
+const PORT = 8000;
+
+//Uso del JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+const pm = new ProductManager();
+
+//Ruta para obtener todos los productos con un límite opcional.
+app.get('/products', async (req, res) => {
+    try{
+        let limit = req.query.limit;
+        let products = await pm.getProducts();
+        if (limit) {
+            let prodLimit = products.slice(0, parseInt(limit));
+            res.send(prodLimit);
+        }else {
+            res.send(products);
+        }
+    }catch (error) {
+        res.status(418).send({error, message:"no funca"});
+    }
+});
+
+//Ruta para obtener un producto especifico por su id
+app.get('/products/:pid', async (req, res) => {
+    try{
+        let ProdId = req.params.pid;
+        const product = await pm.getProductById(Number(ProdId));
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).send({status: "error", message: "No se encuentra el ID"});
+        }
+    } catch (error) {
+        res.status(418).send({error, message:"no funca"})
+    }
+});
+
+//Iniciar server
+app.listen(PORT, () => {
+    console.log(`Server funcionando en Puerto ${PORT}.`);
+});
